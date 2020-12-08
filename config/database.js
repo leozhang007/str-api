89ad2b75fe0a0822aa4d9265fa48f9d129aa40b2
LeoZhang <117000082@qq.com>
2020-12-08 22:31:34 +0800

    gridsome

--- config/database.js
+++ config/database.js
@@ -25,7 +25,7 @@ module.exports = ({ env }) => ({
         port: env.int('DATABASE_PORT', 3306),
         database: env('DATABASE_NAME', 'blog'),
         username: env('DATABASE_USERNAME', 'root'),
-        password: env('DATABASE_PASSWORD', '6w=fBuVhxTsR'),
+        password: env('DATABASE_PASSWORD', 'Root12345'),
       },
       options: {},
     },
